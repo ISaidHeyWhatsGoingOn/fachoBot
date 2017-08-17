4f3967e74330b51c5bd32d83a510a9d7f97915d6
ISaidHeyWhatsGoingOn <31084029+ISaidHeyWhatsGoingOn@users.noreply.github.com>
2017-08-16 21:34:21 -0300

    Merge

---
+++ src/main/java/Commands.java
@@ -0,0 +1,11 @@
+public class Commands {
+    public static final String commandInitChar = "/";
+    public static final String facho = commandInitChar + "facho";
+    public static final String recomendarFrase = commandInitChar + "recomendarFrase";
+    public static final String macri = commandInitChar + "macri";
+    public static final String help = commandInitChar + "help";
+    public static final String uploadCommand = commandInitChar + "upload";
+    public static final String startCommand = commandInitChar + "start";
+    public static final String listCommand = commandInitChar + "list";
+    public static final String STOPCOMMAND = commandInitChar + "stop";
+}
